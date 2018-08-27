d84b1c95153c3b24ca3811e402bf648889715fc5
Arjun Sreedharan <asreedharan@pivotal.io>
2018-08-27 16:31:55 -0400

    Bump libbuildpack to master
    compile-based implementation is passing
    
    Signed-off-by: Micah Young <myoung@pivotal.io>
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,6 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-GOOS=windows go build -o bin/supply.exe hwc/supply/cli
-GOOS=windows go build -o bin/finalize.exe hwc/finalize/cli
+GOOS=windows go build -o bin/compile.exe hwc/compile/cli
+#GOOS=windows go build -o bin/supply.exe hwc/supply/cli
+#GOOS=windows go build -o bin/finalize.exe hwc/finalize/cli
